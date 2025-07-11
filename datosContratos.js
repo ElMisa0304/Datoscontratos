846a946fa0902537a112259edccd5a51d7743cba
ElMisa0304 <misaelilagor@gmail.com>
2025-07-11 10:45:49 -0600

    datosContratos.js

--- datosContratos.js
+++ datosContratos.js
@@ -1,164 +1,188 @@
 {
-    "ISEM-CS-025-2025": {
-        "folios": {
-           "1": {
-               "unidad": "20006415",
-               "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-               "serie": "FORZA 25",
-               "marca": "ECHO SWISSMEX",
-               "inventario": "4E 9580 5234 2010"}
-            "2": {
-                "unidad": "B037803",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 9697 4437 2013"}
-            "3": {
-                "unidad": "B037937",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-            "inventario": "4E 9696 4437 2013"}
-            "4": {
-            "unidad": "B037991",
-            "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-            "serie": "SIN DATO",
-            "marca": "ECHO SWISSMEX",
-            "inventario": "4E 9695 4437 2013"}
-            "5": {
-            "unidad": "B037932",
-            "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-            "serie": "SIN DATO",
-            "marca": "ECHO SWISSMEX",
-            "inventario": "4E 7734 4437 2010"}
-            "6": {
-            "unidad": "B037859",
-            "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-            "serie": "SIN DATO",
-            "marca": "ECHO SWISSMEX",
-            "inventario": "4E 7733 4437 2010"}
-            "7": {
-            "unidad": "1214 513 03 001 001",
-            "equipo": "REFRIGERADOR",
-            "serie": "RBACDV12",
-            "marca": "REFRIMED",
-            "inventario": "1670943"}
-            "8": {
-            "unidad": "1214 513 03 001 001B",
-            "equipo": "REFRIGERADOR",
-            "serie": "RVCCDV-12",
-            "marca": "REFRIMED",
-            "inventario": "1671051"}
-            "9": {
-            "unidad": "1408041512996",
-            "equipo": "REFRIGERADOR",
-            "serie": "REB-450",
-            "marca": "RED FRIO",
-            "inventario": "2E 29499 2267 2014"}
-            "10": {
-            "unidad": "0119-23901-209-034",
-            "equipo": "REFRIGERADOR",
-            "serie": "RVASDV14 ",
-            "marca": "REFRIMED",
-            "inventario": "2E 33197 2267 2020"}
-            "11": {
-            "unidad": "1238 0040 1111 020",
-            "equipo": "ULTRACONGELADOR",
-            "serie": "ULT1386 5 A ",
-            "marca": "THERMO FISHER SCIENTIFIC",
-            "inventario": "1670948"}
-            "12": {
-            "unidad": "1237 8380 1111 003",
-            "equipo": "ULTRACONGELADOR",
-            "serie": "ULT1386 5 A ",
-            "marca": "THERMO FISHER SCIENTIFIC",
-            "inventario": "2E 10 6070 2012"}
-            "13": {
-            "unidad": "VSK4640253",
-            "equipo": "REFRIGERADOR",
-            "serie": "BPR8112BL",
-            "marca": "BLUE POINT",
-            "inventario": "3E 3110 2358 2001"}
-            "14": {
-            "unidad": "UT10608756",
-            "equipo": "CONGELADOR",
-            "serie": "RF-EHLI",
-            "marca": "RF-EHL",
-            "inventario": "2E 34801 2267 2021"}
-            "15": {
-            "unidad": "KOI-00023",
-            "equipo": "REFRIGERADOR",
-            "serie": "R14  ",
-            "marca": "TOREY ",
-            "inventario": "2B 10644 2269 2002"}
-            "16": {
-            "unidad": "77486851-X22AQ42627",
-            "equipo": "PLANTA DE EMERGENCIA",
-            "serie": "12063D0361C0630",
-            "marca": "PLANELEC",
-            "inventario": "ADOHIC001"}
-            "17": {
-            "unidad": "MP-1315-301122025",
-            "equipo": "SISTEMA DE VACIO",
-            "serie": "MP-1315",
-            "marca": "INDUSTRONIC",
-            "inventario": "ADOHIC008                ADOHIC009                ADOHIC010                         ADOHIC011                 ADOHIC012          "}
-            "18": {
-            "unidad": "50220030210013",
-            "equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
-            "serie": "CLCPEURO09",
-            "marca": "TRANE",
-            "inventario": "ADOHIC019"}
-            "19": {
-            "unidad": "50220030210019",
-            "equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
-            "serie": "CLCPEURO20",
-            "marca": "TRANE",
-            "inventario": "ADOHIC020"}
-            "20": {
-            "unidad": "50220030210015",
-            "equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
-            "serie": "CLCPEURO12",
-            "marca": "TRANE",
-            "inventario": "ADOHIC021"}
-            "21": {
-            "unidad": "L89707530",
-            "equipo": "TABLERO DE TRANSFERENCIA",
-            "serie": "QEDC126XW",
-            "marca": "SQUARE D",
-            "inventario": "ADO HIC34"}
-            "22": {
-            "unidad": "23-3125-002",
-            "equipo": "SISTEMA DE AIRE GRADO MEDICO",
-            "serie": "W125MX",
-            "marca": "AIRGMED",
-            "inventario": "ADOHIC025                ADOHIC026                ADOHIC027              ADOHIC028             ADOHIC029 "}
-            "23": {
-            "unidad": "SIN DATO",
-            "equipo": "SISTEMA DE SUBESTACION ELECTRICA",
-            "serie": "FE-A61-N3R-23KV",
-            "marca": "FORUNO",
-            "inventario": "ADOHIC030                ADOHIC031                ADOHIC032 "}
-            "24": {
-            "unidad": "M14K37912",
-            "equipo": "CAMARA FRIGORIFICA",
-            "serie": "LET240CCS",
-            "marca": "BHON",
-            "inventario": "1672752"}
-            "25": {
-            "unidad": "0215 060 01 001 001",
-            "equipo": "CONGELADOR",
-            "serie": "CVASD 20",
-            "marca": "REFRIMED",
-            "inventario": "1670944"}
-            "26": {
-            "unidad": "1214 513 03 001 001A",
-            "equipo": "CONGELADOR",
-            "serie": "CVASD 12",
-            "marca": "REFRIMED",
-            "inventario": "1670945"}
-
-
-        }
-    }
+	"ISEM-CS-025-2025": {
+		"folios": {
+			"1": {
+				"unidad": "20006415",
+				"equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
+				"serie": "FORZA 25",
+				"marca": "ECHO SWISSMEX",
+				"inventario": "4E 9580 5234 2010"
+			},
+			"2": {
+				"unidad": "B037803",
+				"equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
+				"serie": "SIN DATO",
+				"marca": "ECHO SWISSMEX",
+				"inventario": "4E 9697 4437 2013"
+			},
+			"3": {
+				"unidad": "B037937",
+				"equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
+				"serie": "SIN DATO",
+				"marca": "ECHO SWISSMEX",
+				"inventario": "4E 9696 4437 2013"
+			},
+			"4": {
+				"unidad": "B037991",
+				"equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
+				"serie": "SIN DATO",
+				"marca": "ECHO SWISSMEX",
+				"inventario": "4E 9695 4437 2013"
+			},
+			"5": {
+				"unidad": "B037932",
+				"equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
+				"serie": "SIN DATO",
+				"marca": "ECHO SWISSMEX",
+				"inventario": "4E 7734 4437 2010"
+			},
+			"6": {
+				"unidad": "B037859",
+				"equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
+				"serie": "SIN DATO",
+				"marca": "ECHO SWISSMEX",
+				"inventario": "4E 7733 4437 2010"
+			},
+			"7": {
+				"unidad": "1214 513 03 001 001",
+				"equipo": "REFRIGERADOR",
+				"serie": "RBACDV12",
+				"marca": "REFRIMED",
+				"inventario": "1670943"
+			},
+			"8": {
+				"unidad": "1214 513 03 001 001B",
+				"equipo": "REFRIGERADOR",
+				"serie": "RVCCDV-12",
+				"marca": "REFRIMED",
+				"inventario": "1671051"
+			},
+			"9": {
+				"unidad": "1408041512996",
+				"equipo": "REFRIGERADOR",
+				"serie": "REB-450",
+				"marca": "RED FRIO",
+				"inventario": "2E 29499 2267 2014"
+			},
+			"10": {
+				"unidad": "0119-23901-209-034",
+				"equipo": "REFRIGERADOR",
+				"serie": "RVASDV14 ",
+				"marca": "REFRIMED",
+				"inventario": "2E 33197 2267 2020"
+			},
+			"11": {
+				"unidad": "1238 0040 1111 020",
+				"equipo": "ULTRACONGELADOR",
+				"serie": "ULT1386 5 A ",
+				"marca": "THERMO FISHER SCIENTIFIC",
+				"inventario": "1670948"
+			},
+			"12": {
+				"unidad": "1237 8380 1111 003",
+				"equipo": "ULTRACONGELADOR",
+				"serie": "ULT1386 5 A ",
+				"marca": "THERMO FISHER SCIENTIFIC",
+				"inventario": "2E 10 6070 2012"
+			},
+			"13": {
+				"unidad": "VSK4640253",
+				"equipo": "REFRIGERADOR",
+				"serie": "BPR8112BL",
+				"marca": "BLUE POINT",
+				"inventario": "3E 3110 2358 2001"
+			},
+			"14": {
+				"unidad": "UT10608756",
+				"equipo": "CONGELADOR",
+				"serie": "RF-EHLI",
+				"marca": "RF-EHL",
+				"inventario": "2E 34801 2267 2021"
+			},
+			"15": {
+				"unidad": "KOI-00023",
+				"equipo": "REFRIGERADOR",
+				"serie": "R14  ",
+				"marca": "TOREY ",
+				"inventario": "2B 10644 2269 2002"
+			},
+			"16": {
+				"unidad": "77486851-X22AQ42627",
+				"equipo": "PLANTA DE EMERGENCIA",
+				"serie": "12063D0361C0630",
+				"marca": "PLANELEC",
+				"inventario": "ADOHIC001"
+			},
+			"17": {
+				"unidad": "MP-1315-301122025",
+				"equipo": "SISTEMA DE VACIO",
+				"serie": "MP-1315",
+				"marca": "INDUSTRONIC",
+				"inventario": "ADOHIC008                ADOHIC009                ADOHIC010                         ADOHIC011                 ADOHIC012          "
+			},
+			"18": {
+				"unidad": "50220030210013",
+				"equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
+				"serie": "CLCPEURO09",
+				"marca": "TRANE",
+				"inventario": "ADOHIC019"
+			},
+			"19": {
+				"unidad": "50220030210019",
+				"equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
+				"serie": "CLCPEURO20",
+				"marca": "TRANE",
+				"inventario": "ADOHIC020"
+			},
+			"20": {
+				"unidad": "50220030210015",
+				"equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
+				"serie": "CLCPEURO12",
+				"marca": "TRANE",
+				"inventario": "ADOHIC021"
+			},
+			"21": {
+				"unidad": "L89707530",
+				"equipo": "TABLERO DE TRANSFERENCIA",
+				"serie": "QEDC126XW",
+				"marca": "SQUARE D",
+				"inventario": "ADO HIC34"
+			},
+			"22": {
+				"unidad": "23-3125-002",
+				"equipo": "SISTEMA DE AIRE GRADO MEDICO",
+				"serie": "W125MX",
+				"marca": "AIRGMED",
+				"inventario": "ADOHIC025                ADOHIC026                ADOHIC027              ADOHIC028             ADOHIC029 "
+			},
+			"23": {
+				"unidad": "SIN DATO",
+				"equipo": "SISTEMA DE SUBESTACION ELECTRICA",
+				"serie": "FE-A61-N3R-23KV",
+				"marca": "FORUNO",
+				"inventario": "ADOHIC030                ADOHIC031                ADOHIC032 "
+			},
+			"24": {
+				"unidad": "M14K37912",
+				"equipo": "CAMARA FRIGORIFICA",
+				"serie": "LET240CCS",
+				"marca": "BHON",
+				"inventario": "1672752"
+			},
+			"25": {
+				"unidad": "0215 060 01 001 001",
+				"equipo": "CONGELADOR",
+				"serie": "CVASD 20",
+				"marca": "REFRIMED",
+				"inventario": "1670944"
+			},
+			"26": {
+				"unidad": "1214 513 03 001 001A",
+				"equipo": "CONGELADOR",
+				"serie": "CVASD 12",
+				"marca": "REFRIMED",
+				"inventario": "1670945"
+			}
+		}
+	}
 }
