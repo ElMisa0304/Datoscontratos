de64c005a1d87ff6b476ded31091609e794ecae4
ElMisa0304 <misaelilagor@gmail.com>
2025-07-28 10:09:23 -0600

    datosContratos.js

--- datosContratos.js
+++ datosContratos.js
@@ -1,189 +1,35 @@
 {
     "N/A": {
-             "folios": {
+        "folios": {
             "1": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA ECATEPEC",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "FORZA 25",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 9580 5234 2010"
+                "unidad": "---",
+                "equipo": "---",
+                "serie": "---",
+                "marca": "---",
+                "inventario": "---"
             },
             "2": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA TEOTIHUACAN",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 9697 4437 2013"
+                "unidad": "---",
+                "equipo": "---",
+                "serie": "---",
+                "marca": "---",
+                "inventario": "---"
             },
             "3": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA TEOTIHUACAN",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 9696 4437 2013"
+                "unidad": "---",
+                "equipo": "---",
+                "serie": "---",
+                "marca": "---",
+                "inventario": "---"
             },
             "4": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA TEOTIHUACAN",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 9695 4437 2013"
-            },
-            "5": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA TEOTIHUACAN",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 7734 4437 2010"
-            },
-            "6": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA TEOTIHUACAN",
-                "equipo": "MOTO ASPERSORA (MOTOBOMBA) ",
-                "serie": "SIN DATO",
-                "marca": "ECHO SWISSMEX",
-                "inventario": "4E 7733 4437 2010"
-            },
-            "7": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "REFRIGERADOR",
-                "serie": "RBACDV12",
-                "marca": "REFRIMED",
-                "inventario": "1670943"
-            },
-            "8": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "REFRIGERADOR",
-                "serie": "RVCCDV-12",
-                "marca": "REFRIMED",
-                "inventario": "1671051"
-            },
-            "9": {
-                "unidad": "JURISDICCIÓN SANITARIA IXTLAHUACA",
-                "equipo": "REFRIGERADOR",
-                "serie": "REB-450",
-                "marca": "RED FRIO",
-                "inventario": "2E 29499 2267 2014"
-            },
-            "10": {
-                "unidad": "JURISDICCIÓN SANITARIA IXTLAHUACA",
-                "equipo": "REFRIGERADOR",
-                "serie": "RVASDV14 ",
-                "marca": "REFRIMED",
-                "inventario": "2E 33197 2267 2020"
-            },
-            "11": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "ULTRACONGELADOR",
-                "serie": "ULT1386 5 A ",
-                "marca": "THERMO FISHER SCIENTIFIC",
-                "inventario": "1670948"
-            },
-            "12": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "ULTRACONGELADOR",
-                "serie": "ULT1386 5 A ",
-                "marca": "THERMO FISHER SCIENTIFIC",
-                "inventario": "2E 10 6070 2012"
-            },
-            "13": {
-                "unidad": "JURISDICCION DE REGULACION SANITARIA JILOTEPEC",
-                "equipo": "REFRIGERADOR",
-                "serie": "BPR8112BL",
-                "marca": "BLUE POINT",
-                "inventario": "3E 3110 2358 2001"
-            },
-            "14": {
-                "unidad": "JURISDICCIÓN SANITARIA IXTLAHUACA",
-                "equipo": "CONGELADOR",
-                "serie": "RF-EHLI",
-                "marca": "RF-EHL",
-                "inventario": "2E 34801 2267 2021"
-            },
-            "15": {
-                "unidad": "JURISDICCIÓN SANITARIA IXTLAHUACA",
-                "equipo": "REFRIGERADOR",
-                "serie": "R14  ",
-                "marca": "TOREY ",
-                "inventario": "2B 10644 2269 2002"
-            },
-            "16": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "PLANTA DE EMERGENCIA",
-                "serie": "12063D0361C0630",
-                "marca": "PLANELEC",
-                "inventario": "ADOHIC001"
-            },
-            "17": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "SISTEMA DE VACIO",
-                "serie": "MP-1315",
-                "marca": "INDUSTRONIC",
-                "inventario": "ADOHIC008 \nADOHIC009 \nADOHIC010 \nADOHIC011 \nADOHIC012          "
-            },
-            "18": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
-                "serie": "CLCPEURO09",
-                "marca": "TRANE",
-                "inventario": "ADOHIC019"
-            },
-            "19": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
-                "serie": "CLCPEURO20",
-                "marca": "TRANE",
-                "inventario": "ADOHIC020"
-            },
-            "20": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "SISTEMA DE AIRE ACONDICIONADO CON UNIDAD PAQUETE",
-                "serie": "CLCPEURO12",
-                "marca": "TRANE",
-                "inventario": "ADOHIC021"
-            },
-            "21": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "TABLERO DE TRANSFERENCIA",
-                "serie": "QEDC126XW",
-                "marca": "SQUARE D",
-                "inventario": "ADO HIC34"
-            },
-            "22": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "SISTEMA DE AIRE GRADO MEDICO",
-                "serie": "W125MX",
-                "marca": "AIRGMED",
-                "inventario": "ADOHIC025\nADOHIC026\nADOHIC027\nADOHIC028\nADOHIC029 "
-            },
-            "23": {
-                "unidad": "HOSPITAL INTEGRAL COACALCO",
-                "equipo": "SISTEMA DE SUBESTACION ELECTRICA",
-                "serie": "FE-A61-N3R-23KV",
-                "marca": "FORUNO",
-                "inventario": "ADOHIC030\nADOHIC031\nADOHIC032 "
-            },
-            "24": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "CAMARA FRIGORIFICA",
-                "serie": "LET240CCS",
-                "marca": "BHON",
-                "inventario": "1672752"
-            },
-            "25": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "CONGELADOR",
-                "serie": "CVASD 20",
-                "marca": "REFRIMED",
-                "inventario": "1670944"
-            },
-            "26": {
-                "unidad": "BANCO DE SANGRE REGIONAL ECATEPEC LAS AMERICAS",
-                "equipo": "CONGELADOR",
-                "serie": "CVASD 12",
-                "marca": "REFRIMED",
-                "inventario": "1670945"
+                "unidad": "---",
+                "equipo": "---",
+                "serie": "---",
+                "marca": "---",
+                "inventario": "---"
             }
-        }   
+        }
     },
     "ISEM": {
         "folios": {
